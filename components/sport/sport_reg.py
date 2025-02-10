db30d42d37438cea0395480eafb6911a4526e9a7
Savva Ponomarev <s.ponomarev@innopolis.university>
2025-02-10 19:33:09 +0300

    sport fix

--- components/sport/sport_reg.py
+++ components/sport/sport_reg.py
@@ -7,6 +7,7 @@ from tg_client import get_client
 prev_button = None
 
 async def sport_reg(chat_id, day, sport, time):
+    # print(f"Chat id in sport registration{chat_id}")
     client = get_client()
     print("start sport registration")
     last_message = await client.get_messages(chat_id, limit=1)
@@ -34,15 +35,26 @@ async def sport_reg(chat_id, day, sport, time):
 
 async def day_chooser(client, chat_id, day, sport, time):
     # sleep(0.1)
+    # print(f"Chat id in sport day chooser registration{chat_id}")
+    
+    days = {
+        "Monday": "Понедельник",
+        "Tuesday": "Вторник",
+        "Wednesday": "Среда",
+        "Thursday": "Четверг",
+        "Friday": "Пятница",
+        "Saturday": "Суббота",
+        "Sunday": "Воскресенье"
+    }
+
     print("start day choosing")
     last_message = await client.get_messages(chat_id, limit=1)
     last_message = last_message[0]
     message_buttons_rows = last_message.reply_markup.rows.copy()
-    message_buttons_rows = message_buttons_rows[:-1]
-    for row in message_buttons_rows:
+    for row in message_buttons_rows[::-1]:
         for button in row.buttons:
-            # print(f"button: {button.text}")
-            if day in button.text:
+            print(f"button: {button.text}")
+            if day in button.text or days[day] in button.text:
                 print(f"the day: {button.text}, is selected")
                 result = await client(GetBotCallbackAnswerRequest(
                         peer=chat_id,
@@ -51,9 +63,9 @@ async def day_chooser(client, chat_id, day, sport, time):
                     ))
                 print("Result of callback:", result)
                 await session_reg(client, chat_id, sport, time)
-            return
+                return
 async def session_reg(client, chat_id, sport, time):
-    # sleep(0.1)
+
     print("start session registration")
     last_message = await client.get_messages(chat_id, limit=1)
     last_message = last_message[0]
@@ -62,6 +74,8 @@ async def session_reg(client, chat_id, sport, time):
         for button in row.buttons:
             # print(f"for {sport} and button {button.text}: {button.text==sport}")
             if button.text == sport and time in prev_button.text:
+                if "🟢" in prev_button.text:
+                    return "i've already registered"
                 print(f"sesion: {button.text}, time: {time}")
                 try: 
                     result = await client(GetBotCallbackAnswerRequest(
